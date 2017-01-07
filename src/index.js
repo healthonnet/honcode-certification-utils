3ba3b0b9e216397f820a0123ed48bef599386527
William Belle <william.belle@gmail.com>
2017-01-07 18:08:34 +0100

    implement format url function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+exports.formatUrl = function(url) {
+  if (url.indexOf('http://') === 0) {
+    url = url.substr(7);
+  }
+
+  // Supprimer "https://"
+  else if (url.indexOf('https://') === 0) {
+    url = url.substr(8);
+  }
+
+  if (url.indexOf('www.') === 0) {
+    url = url.substr(4);
+  }
+
+  return url;
+};
--- test/test.js
+++ test/test.js
@@ -1,1 +1,15 @@
 'use strict';
+
+var should = require('chai').should();
+var utils  = require('../src/index.js');
+
+describe('HONcode Certification Utils', function() {
+
+  it('should format url', function() {
+    utils.formatUrl('https://www.hon.ch').should.equal('hon.ch');
+    utils.formatUrl('http://www.hon.ch').should.equal('hon.ch');
+    utils.formatUrl('https://hon.ch').should.equal('hon.ch');
+    utils.formatUrl('http://hon.ch').should.equal('hon.ch');
+    utils.formatUrl('').should.equal('');
+  });
+});
